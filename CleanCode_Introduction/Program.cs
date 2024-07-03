aeb018607ea70277f1b5bf851bd8b89e9df25e70
Jonathan Mendoza <john.2114@hotmail.com>
2024-07-03 22:35:22 +0200

    Add project files.

---
+++ CleanCode_Introduction/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//bad
+int n = 0;
+string s = "John";
+
+// good
+int studentCount = 100;
+string studentName = "John";
+
+class CostumerService() // PascalCase
+{
+    public const int MAX_CUSTOMER = 100; //ALL_CAPS
+    public int CustumerCount { get; set; } // PascalCase
+
+    private string lastCustomerName = "John"; // camelCase
+    public string GetCustumerName(int customerId)// camelCase
+    {
+        string customerName = "John Doe";// camelcaseb
+        return customerName;
+    }
+}
---
+++ Special Convention for naming private fields/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Reflection.Emit;
+
+namespace Special_Convention_for_naming_private_fields
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+
+
+        class CustomerService
+        {
+            private string _customerName = "JohnDoe";
+            public CustomerService(string customerName)
+            {
+                _customerName = customerName;
+            }
+
+            // bad
+
+            public void Save()
+            {
+
+            }
+
+            // Good
+            public void SaveCustomer()
+            {
+
+            }public void SaveCustomerName()
+            {
+
+            }
+
+            //much better
+            //Is, Get, Set, Has, Can
+
+
+            public void SetCustomerName()
+            {
+
+            }
+
+            public bool hasErrors()
+            {
+
+                return true; 
+            
+            }
+        }
+
+        }
+
+
+        class OrderProcessor
+        {
+
+
+        }
+        
+       
+
+
+
+    }
+}
